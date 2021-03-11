c442ba2e018b4a6e70a16541183b3683acaddc4e
tehilarefaeli <tehila1728@gmail.com>
2021-03-11 12:09:43 +0200

    tehila

--- our-brothers-web/src/app/shared/services/user.service.ts
+++ our-brothers-web/src/app/shared/services/user.service.ts
@@ -1,10 +1,23 @@
 import { User } from '../../../../../types/models/index';
 
 import { Injectable } from '@angular/core';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   public user: User;
+
+  private fbuSubject: BehaviorSubject<User> = new BehaviorSubject<User>(null);
+  public firebaseTellUser: Observable<User> = this.fbuSubject.asObservable();
+
+  public setFirebaseTellUser(u: User): void {
+    console.log('UserService setFirebaseTellUser, calling next', u);
+    this.fbuSubject.next(u);
+  }
+
+  constructor() {
+    this.firebaseTellUser.subscribe(u => console.log('service test user sub', u));
+  }
 }
